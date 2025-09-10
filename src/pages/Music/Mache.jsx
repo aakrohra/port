44da5e114cb584776cb238dadbe2b406cc6b224d
Aakaash Rohra <57458928+aakrohra@users.noreply.github.com>
2025-09-10 14:53:41 -0400

    fix mache accents

--- src/pages/Music/Mache.jsx
+++ src/pages/Music/Mache.jsx
@@ -7,10 +7,10 @@ function Mache() {
         <div className="band">
             <img src={bandImage} />
             <p className='bandText'>
-                mâché is an indie rock band I joined when it started 2 years ago. It taught me a great deal
+                mache is an indie rock band I joined when it started 2 years ago. It taught me a great deal
                 about collaborating with new people, and introduced me to a vibrant local creative community.
                 The band’s growing local presence has helped me build a network of creatives that I tap into frequently.
-                <br></br><br></br>mâché has played a number of benefit, charity, and diversity shows.<br></br><br></br>Photo by Olivia Dans.
+                <br></br><br></br>mache has played a number of benefit, charity, and diversity shows.<br></br><br></br>Photo by Olivia Dans.
             </p>
         </div>
     )
