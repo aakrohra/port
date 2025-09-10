1a85b15ba8139800d8e110d5c57d7d5f39c1edf7
Aakaash Rohra <57458928+aakrohra@users.noreply.github.com>
2025-09-10 15:50:19 -0400

    pfp and more random additions

--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -1,12 +1,21 @@
 import './Head.css';
 
+import React, { useState } from 'react';
+
+import headshot from '../../assets/headshot.jpg';
+
 function Head() {
+  const [pfp, setPfp] = useState(false);
+  function pfpClick() {
+    setPfp(prev => !prev);
+  }
   return (
     <div>
       <div className='all'>
         <span className="header">Sensory Memory </span>
-        <span className="subheader">Aakaash Rohra's Digital Portfolio </span>
+        <span className="subheader"><a className="clickabletext" onClick={pfpClick}>Aakaash Rohra</a>'s Digital Portfolio </span>
       </div>
+      {pfp && <div className="headshot"><img src={headshot}/></div>}
     </div>
   );
 }
