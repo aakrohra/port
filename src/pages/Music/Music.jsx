88d27782177fbbdbb4d1e8c28c683c0f2d2a0b44
Aakaash Rohra <57458928+aakrohra@users.noreply.github.com>
2025-09-10 14:44:25 -0400

    slight modifications and now full local site is deployable to gh-pages

--- src/pages/Music/Music.jsx
+++ src/pages/Music/Music.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import NavBar from "../../components/NavBar/NavBar";
 
 const musicBar = [{to: "/music/inf", label: "In Framings"},
-                  {to: "/music/mache", label: "mâché"},
+                  {to: "/music/mache", label: "mache"},
                   {to: "/music/derwin", label: "Derwin"}]
 
 function Music() {
