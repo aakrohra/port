1875852892585deb9205146411d2ce1641e44b87
Aakaash Rohra <57458928+aakrohra@users.noreply.github.com>
2025-09-10 14:36:24 -0400

    remaining source files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Head from './components/Head/Head';
+import NavBar from "./components/NavBar/NavBar";
+import CS from "./pages/CS/CS";
+import Music from "./pages/Music/Music";
+import Art from './pages/Art/Art';
+import Home from './pages/Home/Home';
+import Footer from "./components/Footer/Footer";
+import InFramings from './pages/Music/InFramings';
+import Derwin from './pages/Music/Derwin';
+import Mache from './pages/Music/Mache';
+import MusicBlob from './pages/Music/MusicBlob';
+import MarioProject from "./pages/CS/MarioProject";
+import TriviaProject from "./pages/CS/TriviaProject";
+import CSBlob from './pages/CS/CSBlob';
+
+const homeLinks = [{to: "/", label: "Home"},
+                   {to: "/cs", label: "CS"},
+                   {to: "/music", label: "Music"},
+                   {to: "/art", label: "Art/Graphics"}]
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Head />
+      <NavBar navlinks={homeLinks} />
+      <main style={{ flex: 1 }}>
+        <div className="vertical-line"></div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="/cs" element={<CS />}>
+            <Route path='/cs' element={<CSBlob />} />
+            <Route path='/cs/quiz90' element={<TriviaProject />} />
+            <Route path='/cs/drmario' element={<MarioProject />} />
+          </Route>
+          <Route path="/music" element={<Music />}>
+            <Route path='/music' element={<MusicBlob />} />
+            <Route path='/music/inf' element={<InFramings />} />
+            <Route path="/music/mache" element={<Mache />} />
+            <Route path="/music/derwin" element={<Derwin />} />
+          </Route>
+          <Route path='/art' element={<Art />} />
+        </Routes>
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
